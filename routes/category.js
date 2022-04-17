09df7a8dd6c982c52413c901fe26523e14940b9a
yukai pc <1079869931@qq.com>
2022-04-17 23:25:07 +0800

    update

--- routes/category.js
+++ routes/category.js
@@ -1,6 +1,7 @@
 const router = require('koa-router')();
 const sqlServices = require('../serives/category.service').sqlServices;
 const pathName = require('../config/config').pathName;
+const koaRequest = require('koa2-request')
 router.prefix(`${pathName}/category`)
 
 /**
@@ -17,6 +18,27 @@ router.get('/getIconList', async (ctx, next) => {
 router.post('/getSeckill', async (ctx, next) => {
   let result = await sqlServices.querySeckillList();
   ctx.body = result;
+})
+
+router.post('/getKnowdgeData', async (ctx, next) => {
+  console.log(ctx.request.body)
+  const { name } = ctx.request.body
+  if (name) {
+    let res = koaRequest({
+      url: 'http://192.168.0.103:8868/getKnowledge',
+      method: 'get',
+      qs: {
+        name: name
+      }
+    });
+    let result = await res;
+    if(result){
+      result.forEach(element => {
+        console.log(element.str)
+      });
+    }
+    ctx.body = JSON.parse(result.body).content.result
+  }
 })
 
 module.exports = router
